9eb11546c9eca76b6d4dcc40ccc99fc16538a372
Legionofone <richard@rhc2.com>
2024-06-27 08:22:18 -0500

    Update server.js
    added URL variable if not on localhost.

--- public/server.js
+++ public/server.js
@@ -10,7 +10,8 @@ require('dotenv').config();
 
 const client_id = process.env.CLIENT_ID; // Your client id
 const client_secret = process.env.CLIENT_SECRET; // Your secret
-const redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+const url = process.env.URL; // URL from env
+const redirect_uri = url + ':8888/callback'; // Your redirect uri
 const stateKey = 'spotify_auth_state';
 
 /**
